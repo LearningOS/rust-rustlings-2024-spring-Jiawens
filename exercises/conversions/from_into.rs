1dda3262dffd2c0d0551bf8ccc3faba94d47fe53
Jiawens <jiawenliu@bupt.edu.cn>
2024-04-10 13:36:18 +0800

    Finish

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,22 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let splited: Vec<&str> = s.split(',').collect();
+        if splited.len() != 2 {
+            return Person::default();
+        }
+        if splited[0].is_empty() || splited[1].parse::<usize>().is_err() || splited[1] == "0" {
+            return Person::default();
+        }
+        Person {
+            name: splited[0].to_string(),
+            age: splited[1].parse::<usize>().unwrap(),
+        }
     }
 }
 
